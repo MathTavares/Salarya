ae25a1de8606817a372a1a1116100769a1279727
Matheus Tavares <Matheus.Tavares@wolterskluwer.com>
2021-06-15 17:59:43 +0200

    Creazione modello busta Mensile

---
+++ Salarya/Salarya/Services/BustaMensile.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Salarya.Services
+{
+   public class BustaMensile
+   {
+      [JsonProperty("Nome")]
+      public string Nome { get; set; }
+
+      [JsonProperty("Mese")]
+      public string Mese { get; set; }
+
+      [JsonProperty("Netto")]
+      public decimal Netto { get; set; }
+
+      [JsonProperty("Lordo")]
+      public decimal Lordo { get; set; }
+
+      [JsonProperty("FerieDovute")]
+      public int FerieDovute { get; set; }
+
+      [JsonProperty("FerieGodute")]
+      public int FerieGodute { get; set; }
+
+      [JsonProperty("OreMaturate")]
+      public int OreMaturate { get; set; }
+
+      [JsonProperty("OreGodute")]
+      public int OreGodute { get; set; }
+   }
+}
--- Salarya/Salarya/Services/RestService.cs
+++ Salarya/Salarya/Services/RestService.cs
@@ -19,9 +19,9 @@ namespace Salarya.Services
 			_client = new HttpClient();
 		}
 
-		public async Task<List<Repository>> GetRepositoriesAsync(string uri)
+		public async Task<List<BustaMensile>> GetRepositoriesAsync(string uri)
 		{
-			List<Repository> repositories = null;
+			List<BustaMensile> repositories = null;
 			try
 			{
 				HttpResponseMessage response = await _client.GetAsync(uri);
@@ -29,7 +29,7 @@ namespace Salarya.Services
 				if (response.IsSuccessStatusCode)
 				{
 					string content = await response.Content.ReadAsStringAsync();
-					repositories = JsonConvert.DeserializeObject<List<Repository>>(content);
+					repositories = JsonConvert.DeserializeObject<List<BustaMensile>>(content);
 				}
 			}
 			catch (Exception ex)
